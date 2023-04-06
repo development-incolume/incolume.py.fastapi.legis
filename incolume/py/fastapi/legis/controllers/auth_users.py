b8099d7178bfffb176dbedee1bac150a8578b2b6
britodfbr <britodfbr@gmail.com>
2023-04-06 14:15:23 -0300

    feat: #4 estruturas para CRUD

--- incolume/py/fastapi/legis/controllers/auth_users.py
+++ incolume/py/fastapi/legis/controllers/auth_users.py
@@ -5,6 +5,10 @@ from incolume.py.fastapi.legis.schemas.user import User as UserSchema
 from passlib.context import CryptContext
 from fastapi.exceptions import HTTPException
 from fastapi import status
+from jose import jwt, JWTError
+from datetime import datetime, timedelta
+from config import settings
+from typing import List
 
 
 crypt_context = CryptContext(schemes=['sha256_crypt'])
@@ -14,7 +18,7 @@ class User:
     def __init__(self, db_session: Session) -> None:
         self.db_session = db_session
 
-    def register(self, user: UserSchema):
+    def register(self, user: UserSchema)->UserModel:
         new_user = UserModel(
             username=user.username,
             password=crypt_context.hash(user.password)
@@ -26,3 +30,30 @@ class User:
         except IntegrityError:
             raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail='User already exists.')
         return new_user
+    
+    def login(self, user:UserSchema, expires_in: int = 30):
+        user_login = self.db_session.query(UserModel).filter_by(username=user.username)
+
+        if user_login is None or crypt_context.verify(user.username, user_login.password):
+            raise HTTPException(status_code=status.HTTP_401_UNAUTORIZED, detail='Invalid username or password')
+        
+        exp = datetime.utcnow() + timedelta(minutes=expires_in)
+        payload = {
+            'sub': user.username,
+            'exp': exp,
+        }
+
+        access_token = jwt.encode(payload, settings.secret_key, algorithm=settings.ALGORITHM)
+
+    def all(self) -> List[UserModel]:
+        pass
+
+    def view(self) -> UserModel:
+        pass
+
+    def delete(self) -> UserModel:
+        pass
+
+    def update(self) -> UserModel:
+        pass
+    
