0fe0a1658903f785fbe0c1999c9d79f67476633c
britodfbr <britodfbr@gmail.com>
2023-04-10 11:31:11 -0300

    feat: #11 sepado ambientes através de envvars
    close #11

--- config.py
+++ config.py
@@ -4,6 +4,8 @@ from dynaconf import Dynaconf
 settings = Dynaconf(
     envvar_prefix="INCOLUME",
     settings_files=['settings.toml', '.secrets.toml'],
+    environments=['development', 'dev0', 'testing'],
+    env_switcher="INCOLUME_MODE"
 )
 
 # `envvar_prefix` = export envvars with `export DYNACONF_FOO=bar`.
